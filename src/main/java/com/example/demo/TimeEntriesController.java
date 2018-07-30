487a5a5e952cfc225d8127598b60dd2dc99761fd
Christian Trutz <info@christian-trutz.de>
2018-07-30 15:39:01 +0200

    Nachdem Spring Data aufgenommen wurde.

--- src/main/java/com/example/demo/TimeEntriesController.java
+++ src/main/java/com/example/demo/TimeEntriesController.java
@@ -1,31 +1,30 @@
 package com.example.demo;
 
 import java.time.LocalDateTime;
-import java.util.LinkedList;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 class TimeEntriesController {
+
+	@Autowired
+	TimeEntriesRepository repository;
 	
-	private List<TimeEntry> timeEntries = new LinkedList<>();
-	
-	public TimeEntriesController() {
-		timeEntries.add(new TimeEntry(LocalDateTime.now()));
-		timeEntries.add(new TimeEntry(LocalDateTime.now()));
-		timeEntries.add(new TimeEntry(LocalDateTime.now()));
-		timeEntries.add(new TimeEntry(LocalDateTime.now()));
-		timeEntries.add(new TimeEntry(LocalDateTime.now()));
+	@GetMapping("/add_timeentry")
+	ModelAndView addTimeentry() {
+		TimeEntry timeEntry = new TimeEntry(LocalDateTime.now());
+		repository.save(timeEntry);
+		return timeentries();
 	}
 
 	@GetMapping("/timeentries")
 	ModelAndView timeentries() {
 		ModelAndView modelAndView = new ModelAndView("timeentries");
 		modelAndView.addObject("title", "Zeiteinträge Anwendung");
-		modelAndView.addObject("timeEntries", timeEntries);
+		modelAndView.addObject("timeEntries", repository.findAll());
 		return modelAndView;
 	}
 
--- src/main/java/com/example/demo/TimeEntry.java
+++ src/main/java/com/example/demo/TimeEntry.java
@@ -1,19 +1,29 @@
 package com.example.demo;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+@Document
 @Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class TimeEntry {
-	
-	private Long id;
-	
-	private final LocalDateTime start;
-	
-	private LocalDate stop;
-	
+
+	@Id
+	private String id;
+
+	@NonNull
+	private LocalDateTime start;
+
+	private LocalDateTime stop;
+
 	private String description;
 
 }
